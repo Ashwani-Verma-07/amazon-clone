a890ad48bffa52ada433ec81573b0d8128082f87
Ashwani Verma <89683890+Ashwani-Verma-07@users.noreply.github.com>
2023-10-28 22:33:43 +0530

    hided api keys

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import "firebase/compat/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCBxRBnazqC8NxIa2XAJL4WALENqahmYLo",
+  apiKey: process.env.API_KEY,
   authDomain: "challenge-6bd65.firebaseapp.com",
   databaseURL: "https://challenge-6bd65.firebase.io.com",
   projectId: "challenge-6bd65",
   storageBucket: "challenge-6bd65.appspot.com",
   messagingSenderId: "687533293362",
-  appId: "1:687533293362:web:2b06c6edf0f1e1b461612b",
-  measurementId: "G-M8JM8Y91MR",
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
